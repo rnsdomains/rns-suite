1f773ef30b76e8c2bd826d3b18e2eee59ef3d34b
Ilan Olkies <ilanolkies@outlook.com>
2020-04-20 02:57:29 -0300

    Remove old sample

--- sample/migrations/2_deploy_rns.js
+++
@@ -1,6 +0,0 @@
-const rnsSuite = require('@rsksmart/rns-suite')(artifacts);
-
-module.exports = function(deployer, _, accounts) {
-  deployer.then(async () => await rnsSuite(deployer, accounts));
-};
-
